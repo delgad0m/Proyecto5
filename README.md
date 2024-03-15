03bc2b4e2a447fa65cdd0476b592a1ba07773376
Mate <160535011+delgad0m@users.noreply.github.com>
2024-03-15 13:04:01 -0500

    Confirmar cambios

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Proyecto5
+Este proyecto consiste en el desarrollo de una aplicación web para visualizar y explorar datos de anuncios de venta de coches. Utilizamos Streamlit, Plotly Express y Pandas para construir una interfaz interactiva que permite a los usuarios generar visualizaciones de datos a partir de un conjunto de datos de anuncios de venta de coches.
+
+Construye histogramas para analizar la distribución de variables como el kilometraje.
+Crea gráficos de dispersión para explorar la relación entre variables como el kilometraje y el precio.
+Muestra un interfaz de usuario intuitiva con botones para generar visualizaciones.
--- app.py
+++ app.py
@@ -1,5 +1,32 @@
+import pandas as pd
+import plotly.express as px
 import streamlit as st
 
-st.header('Lanzar una moneda')
+# Lee el archivo CSV del conjunto de datos
+car_data = pd.read_csv('vehicles_us.csv')
 
-st.write('Esta aplicación aún no es funcional. En construcción.')
+# Configuración de la página
+st.title('Cuadro de Mandos de Anuncios de Venta de Coches')
+
+# Encabezado
+st.header('Visualizaciones')
+
+# Botón para construir histograma
+hist_button = st.button('Construir histograma')
+
+# Botón para construir gráfico de dispersión
+scatter_button = st.button('Construir gráfico de dispersión')
+
+# Verificar si se hizo clic en el botón de histograma
+if hist_button:
+    # Crear un histograma
+    fig_hist = px.histogram(car_data, x="odometer")
+    # Mostrar el histograma
+    st.plotly_chart(fig_hist, use_container_width=True)
+
+# Verificar si se hizo clic en el botón de gráfico de dispersión
+if scatter_button:
+    # Crear un gráfico de dispersión
+    fig_scatter = px.scatter(car_data, x="odometer", y="price")
+    # Mostrar el gráfico de dispersión
+    st.plotly_chart(fig_scatter, use_container_width=True)
